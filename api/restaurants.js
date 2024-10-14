ac387043d541fef5dd1d93ea8632bb0bd2b2608c
Edwin <edwincastaneda0801@gmail.com>
2024-10-14 11:11:20 -0500

    end

--- api/restaurants.js
+++ api/restaurants.js
@@ -5,3 +5,50 @@ module.exports = router;
 const prisma = require("../prisma");
 
 // TODO: routes!
+
+router.get("/", async (req, res, next) => {
+  try {
+    const restaurants = await prisma.restaurant.findMany();
+    res.json(restaurants);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const restaurant = await prisma.restaurant.findUniqueOrThrow({
+      where: { id: +id },
+      include: { reservations: true },
+    });
+    if (restaurant) {
+      res.json(restaurant);
+    } else {
+      next({ status: 404, message: `Restaurant with id: ${id} not found` });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/:id/reservations", async (req, res, next) => {
+  const { id } = req.params;
+  const { name, email, partySize } = req.body;
+
+  if (!name || !email || !partySize) {
+    return next({
+      status: 404,
+      message: "You need a name for the restaurant",
+    });
+  }
+  try {
+    const reservation = await prisma.reservation.create({
+      data: { name, email, partySize: +partySize, restaurantId: +id },
+    });
+    res.status(201).json(reservation);
+  } catch (e) {
+    next(e);
+  }
+});
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,34 @@
 const prisma = require("../prisma");
-const seed = async () => {
+const { faker } = require("@faker-js/faker");
+
+const seed = async (numRestaurants = 3, numReservations = 5) => {
   // TODO: Create 3 restaurants with 5 reservations each
+
+  // A loop must be used because `prisma.restaurant.createMany` fails here
+  for (let i = 0; i < numRestaurants; i++) {
+    // For each restaurant, create an array of 5 reservations
+    const reservations = [];
+    for (let j = 0; j < numReservations; j++) {
+      const name = faker.internet.displayName();
+      reservations.push({
+        name,
+        email: `${name}${j}@foo.bar`,
+        partySize: Math.floor(Math.random() * 10) + 1,
+      });
+    }
+
+    // Create a single restaurant with nested reservations
+    await prisma.restaurant.create({
+      data: {
+        name: faker.company.buzzAdjective() + " " + faker.company.buzzNoun(),
+        reservations: {
+          create: reservations,
+        },
+      },
+    });
+  }
 };
+
 seed()
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
